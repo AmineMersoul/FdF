3505d83ff6f46384bc273fc582f1ac7eefead1c1
Amine Mersoul <amine.3dsmax@live.fr>
2018-10-23 18:03:10 +0100

    creating struct for surface

--- wireframe/ft_create_surface.c
+++ wireframe/ft_create_surface.c
@@ -1,42 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_create_wireframe.c                              :+:      :+:    :+:   */
+/*   ft_create_surface.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amersoul <amersoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/21 20:48:10 by amersoul          #+#    #+#             */
-/*   Updated: 2018/10/22 17:11:26 by amersoul         ###   ########.fr       */
+/*   Updated: 2018/10/23 18:02:16 by amersoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wireframe.h"
 
-int		**ft_create_wireframe(const int fd,
-const int x, const int y)
+t_surface		*ft_create_surface(const int fd, const int x, const int y)
 {
 	char	*line;
-	int		i;
-	int		j;
+	int		row;
+	int		col;
 	char	**res;
-	int		**wireframe;
+	t_surface *surface;
 
-	wireframe = (int **)malloc(sizeof(int *) * y);
-	wireframe[0] = (int *)malloc(sizeof(int) * x * y);
-	i = 0;
-	while (++i < y)
-		wireframe[i] = (*wireframe + x * i);
-	i = 0;
+	surface = malloc(sizeof(surface));
+	surface->rows = y;
+	surface->cols = x;
+	surface->t_vertex  = (t_vertex **)malloc(sizeof(t_vertex *) * x); 
+    surface->t_vertex[0] = (t_vertex *)malloc(sizeof(t_vertex) * y * x); 
+    for(row = 0; row < x; row++) 
+        surface->t_vertex[row] = (*surface->t_vertex + y * row);
+	row = 0;
 	while (get_next_line(fd, &line))
 	{
 		res = ft_strsplit((const char*)line, ' ');
-		j = 0;
-		while (res[j] != 0)
+		col = 0;
+		while (res[col] != 0)
 		{
-			wireframe[i][j] = ft_atoi((const char *)res[j]);
-			j++;
+			surface->t_vertex[row][col] = ft_create_vertex(row, col, ft_atoi((const char *)res[col]));
+			col++;
 		}
-		i++;
+		row++;
 	}
-	return (wireframe);
+	return (surface);
 }
--- wireframe/wireframe.h
+++ wireframe/wireframe.h
@@ -6,7 +6,7 @@
 /*   By: amersoul <amersoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/21 14:51:02 by amersoul          #+#    #+#             */
-/*   Updated: 2018/10/22 17:12:01 by amersoul         ###   ########.fr       */
+/*   Updated: 2018/10/23 18:02:30 by amersoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 
 # include "../get_next_line/get_next_line.h"
 # include "../libft/libft.h"
+# include "../draw/draw.h"
 
-void	ft_count_x_y(const int fd, int *x, int *y);
-int		**ft_create_wireframe(const int fd,
-const int x, const int y);
+typedef struct
+{
+	t_vertex	**t_vertex;
+	int			rows;
+	int			cols;
+}				t_surface;
+
+void			ft_count_x_y(const int fd, int *x, int *y);
+t_surface		*ft_create_surface(const int fd, const int x, const int y);
 
 #endif
