c3d3a4477d54cf6e248931354f558d68ce1e0d18
Amine Mersoul <amine.3dsmax@live.fr>
2018-10-23 17:56:18 +0100

    adding create vertex function

---
+++ draw/ft_create_vertex.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_vertex.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amersoul <amersoul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/23 12:12:19 by amersoul          #+#    #+#             */
+/*   Updated: 2018/10/23 12:25:04 by amersoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "draw.h"
+
+t_vertex	ft_create_vertex(const int x, const int y, const int z)
+{
+	t_vertex vertex;
+
+	vertex.x = x;
+	vertex.y = y;
+	vertex.z = z;
+	return (vertex);
+}
