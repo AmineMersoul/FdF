90f2687b9c886f16530916566ce26f92871f14d7
Amine Mersoul <amersoul@e2r5p9.1337.ma>
2018-11-13 19:26:02 +0100

    norm

--- wireframe/ft_count_x_y.c
+++ wireframe/ft_count_x_y.c
@@ -6,7 +6,7 @@
 /*   By: amersoul <amersoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/21 20:44:54 by amersoul          #+#    #+#             */
-/*   Updated: 2018/11/12 16:21:33 by amersoul         ###   ########.fr       */
+/*   Updated: 2018/11/13 19:25:06 by amersoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int	ft_count_x_y(const int fd, int *x, int *y)
 	while (get_next_line(fd, &line))
 	{
 		if (*x != ft_wordcount(line, ' '))
-			return 0;
+			return (0);
 		count++;
 	}
 	*y = count;
-	return 1;
+	return (1);
 }
